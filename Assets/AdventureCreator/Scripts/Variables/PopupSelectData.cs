3e973c87ca140ca51f985727c30381c0b696a382
ChaosTomatoe <santidevargas@gmail.com>
2022-10-03 18:59:55 -0300

    new changes

---
+++ Assets/AdventureCreator/Scripts/Variables/PopupSelectData.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_EDITOR
+
+namespace AC
+{
+
+	public struct PopupSelectData
+	{
+
+		public int ID;
+		public string label;
+		public int rootIndex;
+
+
+		public PopupSelectData (int _ID, string _label, int _rootIndex)
+		{
+			ID = _ID;
+			label = _label;
+			rootIndex = _rootIndex;
+		}
+
+
+		public string EditorLabel
+		{
+			get
+			{
+				return ID.ToString () + ": " + label;
+			}
+		}
+
+	}
+}
+
+#endif
